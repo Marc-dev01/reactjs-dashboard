b86fcf695777375c1c13bb7d77911cffaa4e36b6
Marc-dev01 <marcdev002@gmail.com>
2024-02-08 19:40:00 +0100

    init dashboard route

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <p> Ivoices Page</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Roboto, Montserrat, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '900'],
+});
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '900'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
